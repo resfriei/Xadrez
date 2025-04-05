ad2533d55fd31c7aa75bc574386764f432b3a512
Marllon <marllonandrade567@gmail.com>
2025-04-05 04:51:37 -0300

    Adicionando movimentos do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -13,33 +13,59 @@
 
      for (i = 1; i <= 5; i++) 
      {
-         printf("Torre: %d Direita\n", i);
+         printf("TORRE: %d Direita\n", i);
      }
+
      
-     // Movimentando o Bispo (5 casa na diagonal, para cima e a direita) utilizando while:
+     // Movimentando o Bispo (5 casa na diagonal, para cima e a direita) utilizando while
  
-     printf("\nMovimentando o Bispo 5 casa na diagonal para cima e à direita\n");
+     printf("\nMovimentando o Bispo 5 casas na diagonal para cima e à direita\n");
 
      i = 1;
     
      while (i <= 5)
      {
-        printf("Bispo: %d Cima e à direita\n", i);
+        printf("BISPO: %d Cima e à direita\n", i);
          i++;
      }
+
  
      // Movimentando a Rainha (8 casas para a esquerada) utilizando do-while
 
-     printf("\nMovimentando a Rainha 8 casa na horizontal para à esquerda\n");
+     printf("\nMovimentando a Rainha 8 casas na horizontal para à esquerda\n");
 
      i = 1;
 
      do {
-         printf("Rainha: %d Esquerda\n", i);
+         printf("RAINHA: %d Esquerda\n", i);
          i++; 
      } while (i <= 8);
+
+
+     // Movimentando o Cavalo (duas casas para baixo e uma para a esquerda) utilizando for e while
  
+    int movBaixo = 2;
+    int movEsquerda = 1;
+
+    // Primeiro loop: duas casas para baixo usando for
+
+    printf("\nMovimentando o Cavalo 2 casas para baixo e uma à esquerda\n");
+
+    for (int i = 1; i <= movBaixo; i++) {
+        printf("CAVALO: %d Baixo\n", i);
+    }
+
+    // Segundo loop: uma casa para a esquerda usando while
+
+    int j = 1;
+    while (j <= movEsquerda) {
+        printf("CAVALO: %d Esquerda\n", j);
+        j++;
+    }
+
+
      printf("## Fim do jogo de xadrez! ##\n");
 
      return 0;
+     
  }
